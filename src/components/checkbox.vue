2e8657a3651af11bc8aff7622bed1fdaea47dada
Mike Ludemann <mike-ludemann@hotmail.de>
2020-02-18 15:36:00 +0100

    Setup complete - vue-checkbox

---
+++ src/components/checkbox.vue
@@ -0,0 +1,50 @@
+<template lang="html">
+
+	<div class="custom--checkbox">
+		<input type="checkbox" :value="value" :name="name" :checked="!!isChecked" @change="e => isChecked = e.target.checked">
+	</div>
+
+</template>
+
+<script lang="js">
+
+	export default {
+		name: 'CustomCheckbox',
+		props: {
+			name: {
+				type: string,
+				required: true
+			},
+			value: {
+				type: string,
+				required: true
+			},
+			isChecked: {
+				type: boolean,
+				default: false,
+				required: true
+			}
+		},
+		mounted () {
+
+		},
+		data () {
+			return {
+
+			}
+		},
+		methods: {
+
+		},
+		computed: {
+
+		}
+	}
+
+</script>
+
+<style scoped lang="scss">
+	.checkbox {
+
+	}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <checkbox name="test" value="tester1"></checkbox>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue';
+import CustomCheckbox from '@/components/checkbox.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    CustomCheckbox,
+  }
+}
+</script>
